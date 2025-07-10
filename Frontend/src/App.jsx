ee03714e1d75bb1834303201d1a3d783fd8dd22f
prabhakaranskt <prabhaece104@gmail.com>
2025-07-10 11:29:03 +0530

    Temp name changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Home from "./pages/Home/Home";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Cart from "./pages/Cart/Cart";
-import PlaceOrder from "./pages/tempFolder/";
+import PlaceOrder from "./pages/PlaceOrder";
 import Orders from "./pages/Orders/Orders";
 import Login from "./pages/loginAndRegister/Login";
 import { Navigate } from "react-router-dom";
