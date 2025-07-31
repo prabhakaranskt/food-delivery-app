51d178075554f0aecfbc470cb22398ff9fced5ae
prabhakaranskt <prabhaece104@gmail.com>
2025-07-31 12:51:34 +0530

    All Goods

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -9,7 +9,7 @@ import Orders from "./pages/Orders/Orders";
 import { ToastContainer } from "react-toastify";
 
 const App = () => {
-  const url = import.meta.env.BACKEND_API_URL || "http://localhost:4000";
+  const url = import.meta.env.BACKEND_API_URL ;
 
   return (
     <div>
